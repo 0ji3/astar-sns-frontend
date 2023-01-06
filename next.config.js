12e8a754b6b4228b20a1d8fba5e2ecf808ad3d4a
Naoki Ogawara <naokiogawara@NaokinoMacBook-Air.local>
2023-01-07 00:19:50 +0900

    initial commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "images.unsplash.com", 
+      "unsplash.com", 
+      "plus.unsplash.com",
+      "ibb.co",
+      "i.ibb.co",
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
